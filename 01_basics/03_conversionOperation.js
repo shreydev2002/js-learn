66d707924895dcb9da480d3788f02f003c036a98
shreydev2002 <145108616+shreydev2002@users.noreply.github.com>
2024-01-09 11:26:15 +0000

    conversion of Data types

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,66 @@
+/* File seems to be large but supports investigation study of different cases.
+                
+            !!   NOTES are present beneath.         SCROLL DOWN */ 
+
+// typeof variable  &  typeof(variable)   both will give the same answer.
+
+let score1 = "33"
+console.log(typeof score1);
+let valueInNumber1=Number(score1);
+console.log(typeof valueInNumber1);
+console.log()
+
+let score2= "33abc"
+console.log(typeof score2);
+let valueInNumber2=Number(score2)
+console.log(typeof valueInNumber2)
+console.log(valueInNumber2)
+console.log();
+
+let score3= null
+console.log(typeof score3);
+let valueInNumber3=Number(score3)
+console.log(typeof valueInNumber3)
+console.log(valueInNumber3)
+console.log();
+
+let score4= undefined
+console.log(typeof score4);
+let valueInNumber4=Number(score4)
+console.log(typeof valueInNumber4)
+console.log(valueInNumber4)
+console.log();
+
+let score5= true
+console.log(typeof score5);
+let valueInNumber5=Number(score5)
+console.log(typeof valueInNumber5)
+console.log(valueInNumber5)
+console.log();
+
+let score6= "DEV"
+console.log(typeof score6);
+let valueInNumber6=Number(score6)
+console.log(typeof valueInNumber6)
+console.log(valueInNumber6)
+console.log();
+
+// "33" => 33
+// "33abc" => NaN
+//  true => 1; false => 0
+
+
+let isLoggedIn= 1
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+console.log(booleanIsLoggedIn)
+console.log();
+
+// 1 => true; 0 => false
+// "" => false
+// "hitesh" => true
+
+let somenumber = 33
+let stringNumber = String(somenumber);
+console.log(typeof stringNumber)
+console.log(stringNumber)
+
