2bf23d2a4d3a23a99f9920da106cee70206eca53
shreydev2002 <145108616+shreydev2002@users.noreply.github.com>
2024-01-19 18:41:13 +0000

    If and else if and nested

---
+++ 04_control_flow/one.js
@@ -0,0 +1,43 @@
+//if
+
+temperature = 41
+// if ( temperature === 40 ){
+//     console.log("less than 50");
+// } else {
+//     console.log("temperature is greater than 50");
+// }
+// console.log("Execute");
+
+// <, >, <=, >=, ==, !=, ===, !==
+
+// A set of code inside {} has different scope than the rest code
+
+// if (balance > 500) console.log("test"),console.log("test2");       //implicit
+
+const balance = 1000
+// if (balance < 500) {
+//     console.log("less than 500");
+// } else if (balance < 750) {
+//     console.log("less than 750");
+    
+// } else if (balance < 900) {
+//     console.log("less than 750");
+    
+// } else {
+//     console.log("less than 1200");
+
+// }
+
+
+const userLoggedIn = true
+const debitCard = true
+const loggedInFromGoogle = false
+const loggedInFromEmail = true
+
+// if (userLoggedIn && debitCard && 2==3) {
+//     console.log("Allow to buy course");
+// }
+
+// if (loggedInFromGoogle || loggedInFromEmail) {
+//     console.log("User logged in");
+//  }
