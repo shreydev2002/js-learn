fdf2900aa92e94dd637ff01076a01f2dc2b66d02
shreydev2002 <145108616+shreydev2002@users.noreply.github.com>
2024-02-09 06:05:05 +0000

    While and Do-while loop

---
+++ 05_iterations/two.js
@@ -0,0 +1,24 @@
+//WHILE
+
+let i =1;
+while (i<=10) {
+    // console.log(`Value is ${i}`);
+    i+=2
+}
+
+let myArr=["FLASH","BATMAN","WONDERWOMAN"]
+let len=myArr.length
+let arr = 0
+while(arr<len)
+{
+    // console.log(myArr[arr]);
+    arr++
+}
+
+// DO WHILE
+
+let score=1
+do {
+    // console.log(`Score is ${score}`);
+    score++
+} while (score<=10);
